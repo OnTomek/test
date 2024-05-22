fbd0d974f768c03b7364a0982455c230006e65a4
OnTomek <tomek.on.2021@skola.ssps.cz>
2024-05-22 11:47:38 +0200

    jeste nefacha

--- feature/package.py
+++ feature/package.py
@@ -1,3 +1,4 @@
+"""
 def create_package_file(branch_name):
     filename = f"{branch_name}.package"
     with open(filename, 'w') as f:
@@ -5,17 +6,18 @@ def create_package_file(branch_name):
     print(f"Empty package file '{filename}' created.")
 
 def main():
-    from pr_num import get_branch_name
+    from test.pr_num.py import get_branch_name
     branch_name = get_branch_name()
-    create_package_file(branch_name)
+    create_deployment_folder(branch_name)
 
 if __name__ == "__main__":
     main()
-
+"""
 
 
 """
 import os
+import sys
 
 def create_package_file(branch_name, folder_name):
     filename = os.path.join(folder_name, f"{branch_name}.package")
