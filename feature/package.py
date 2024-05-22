b9fc40df59cc91c152d1e7454cb202ffb570643d
OnTomek <tomek.on.2021@skola.ssps.cz>
2024-05-22 11:00:05 +0200

    work in progress

---
+++ feature/package.py
@@ -0,0 +1,34 @@
+def create_package_file(branch_name):
+    filename = f"{branch_name}.package"
+    with open(filename, 'w') as f:
+        pass  # Create an empty file
+    print(f"Empty package file '{filename}' created.")
+
+def main():
+    from pr_num import get_branch_name
+    branch_name = get_branch_name()
+    create_package_file(branch_name)
+
+if __name__ == "__main__":
+    main()
+
+
+
+"""
+import os
+
+def create_package_file(branch_name, folder_name):
+    filename = os.path.join(folder_name, f"{branch_name}.package")
+    with open(filename, 'w') as f:
+        pass  # Create an empty file
+    print(f"Empty package file '{filename}' created.")
+
+def main():
+    from pr_num import get_branch_name, get_folder_name
+    branch_name = get_branch_name()
+    folder_name = get_folder_name()
+    create_package_file(branch_name, folder_name)
+
+if __name__ == "__main__":
+    main()
+"""
